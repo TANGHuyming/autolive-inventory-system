045e3dcea412cb368c91ac5005870fcddec69fe6
TANGHuyming <2023277tang@aupp.edu.kh>
2026-09-05 17:43:55 +0000

    Fix permission bug

--- src/router/index.ts
+++ src/router/index.ts
@@ -125,7 +125,7 @@ router.beforeEach(async (to) => {
   }
 
   if (to.meta.permission) {
-    const permissions = JSON.stringify(to.meta.permission).split('.')
+    const permissions = JSON.stringify(to.meta.permission).slice(1, -1).split('.')
     const profile = employeeStore.profile || { role: { role_name: '' } }
     if (!permissions.includes(profile.role.role_name)) {
       return '/access-denied'
